80d1cc9def90a95f75cbd86baeb09aab56416905
Kyoichiro Yamada <me@kyoh86.dev>
2020-01-12 17:55:42 +0900

    It's not problem about only Empty String.

---
+++ decode_unmarshalable_string_test.go
@@ -0,0 +1,53 @@
+package yaml
+
+import (
+	"encoding/json"
+	"testing"
+)
+
+type unmarshalableStringValue string
+
+func (v *unmarshalableStringValue) UnmarshalYAML(raw []byte) error {
+	*v = unmarshalableStringValue(string(raw))
+	return nil
+}
+
+type unmarshalableStringContainer struct {
+	V unmarshalableStringValue `yaml:"value" json:"value"`
+}
+
+func TestUnmarshalableString(t *testing.T) {
+	var container unmarshalableStringContainer
+	t.Run("empty string", func(t *testing.T) {
+		if err := Unmarshal([]byte(`value: ""`), &container); err != nil {
+			t.Fatalf("failed to unmarshal %v", err)
+		}
+		if container.V != "" {
+			t.Fatalf("expected empty string, but %q is set", container.V)
+		}
+	})
+	t.Run("filled string", func(t *testing.T) {
+		if err := Unmarshal([]byte(`value: "aaa"`), &container); err != nil {
+			t.Fatalf("failed to unmarshal %v", err)
+		}
+		if container.V != "aaa" {
+			t.Fatalf("expected empty string, but %q is set", container.V)
+		}
+	})
+	t.Run("(json) empty string", func(t *testing.T) {
+		if err := json.Unmarshal([]byte(`{"value": ""}`), &container); err != nil {
+			t.Fatalf("failed to unmarshal %v", err)
+		}
+		if container.V != "" {
+			t.Fatalf("expected empty string, but %q is set", container.V)
+		}
+	})
+	t.Run("(json) filled string", func(t *testing.T) {
+		if err := json.Unmarshal([]byte(`{"value": "aaa"}`), &container); err != nil {
+			t.Fatalf("failed to unmarshal %v", err)
+		}
+		if container.V != "aaa" {
+			t.Fatalf("expected empty string, but %q is set", container.V)
+		}
+	})
+}

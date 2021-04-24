4a3f1dc60994e112d2a1d68908f8d1ca28cbc450
Nikolai Avteniev <nikolai.avteniev@gmail.com>
2021-04-24 18:01:36 -0400

    Added a test case with top level required field

--- validate_test.go
+++ validate_test.go
@@ -39,6 +39,24 @@ func TestStructValidator(t *testing.T) {
 			}{},
 		},
 		{
+			TestName: "Test Missing Required Field",
+			YAMLContent: `---
+- name: john
+  age: 20
+- age: 10`,
+			ExpectedErr: `[4:1] Key: 'Name' Error:Field validation for 'Name' failed on the 'required' tag
+   1 | ---
+   2 | - name: john
+   3 |   age: 20
+>  4 | - age: 10
+       ^
+`,
+			Instance: &[]struct {
+				Name string `yaml:"name" validate:"required"`
+				Age  int    `yaml:"age" validate:"gte=0,lt=120"`
+			}{},
+		},
+		{
 			TestName: "Test Nested Validation Missing Internal Required",
 			YAMLContent: `---
 name: john

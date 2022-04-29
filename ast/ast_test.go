91b0428ddea8df71e688293bf1e4860006ca99fd
Florian Rey <nervo@nervo.net>
2022-04-30 00:27:49 +0200

    UTF-8 Support

--- ast/ast_test.go
+++ ast/ast_test.go
@@ -1,6 +1,10 @@
 package ast
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/goccy/go-yaml/token"
+)
 
 func TestEscapeSingleQuote(t *testing.T) {
 	expected := `'Victor''s victory'`
@@ -8,4 +12,25 @@ func TestEscapeSingleQuote(t *testing.T) {
 	if got != expected {
 		t.Fatalf("expected:%s\ngot:%s", expected, got)
 	}
+}
+
+func TestReadNode(t *testing.T) {
+	t.Run("utf-8", func(t *testing.T) {
+		value := "éɛทᛞ⠻チ▓🦄"
+		node := &StringNode{
+			BaseNode: &BaseNode{},
+			Token:    &token.Token{},
+			Value:    value,
+		}
+		expectedSize := len(value)
+		gotBuffer := make([]byte, expectedSize)
+		expectedBuffer := []byte(value)
+		gotSize, _ := readNode(gotBuffer, node)
+		if gotSize != expectedSize {
+			t.Fatalf("expected size:%d\ngot:%d", expectedSize, gotSize)
+		}
+		if string(gotBuffer) != string(expectedBuffer) {
+			t.Fatalf("expected buffer:%s\ngot:%s", expectedBuffer, gotBuffer)
+		}
+	})
 }

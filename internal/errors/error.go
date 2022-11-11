331b18e72b37b96938df3b57e3855dc8e202c498
Dan Kortschak <dan@kortschak.io>
2022-11-12 07:00:21 +1030

    Don't provide stack trace for sentinel errors
    These values are effectively constants and are documented, so don't
    pollute the string representation of the error with a constant stack
    trace. Also use stdlib errors.Is/As rather than xerrors.Is/As where
    package name collisions allow.

--- internal/errors/error.go
+++ internal/errors/error.go
@@ -2,6 +2,7 @@ package errors
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
 	"reflect"
 
@@ -83,7 +84,7 @@ func (e *wrapError) As(target interface{}) bool {
 		}
 		break
 	}
-	return xerrors.As(err, target)
+	return errors.As(err, target)
 }
 
 func (e *wrapError) Unwrap() error {

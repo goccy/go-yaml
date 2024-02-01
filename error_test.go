cc4ec165eb72f89298185f44c7984dc9874e5aeb
Mateusz Hawrus <mateusz.hawrus@nobl9.com>
2024-02-01 19:17:19 +0100

    add tests

---
+++ error_test.go
@@ -0,0 +1,54 @@
+package yaml
+
+import (
+	"testing"
+
+	"golang.org/x/xerrors"
+
+	"github.com/goccy/go-yaml/internal/errors"
+	"github.com/goccy/go-yaml/token"
+)
+
+func TestAsSyntaxError(t *testing.T) {
+	tests := []struct {
+		input    error
+		expected *SyntaxError
+	}{
+		{
+			input:    nil,
+			expected: nil,
+		},
+		{
+			input:    xerrors.New("dummy test"),
+			expected: nil,
+		},
+		{
+			input: errors.ErrSyntax("some error", &token.Token{Value: "123"}),
+			expected: &SyntaxError{
+				Msg:   "some error",
+				Token: &token.Token{Value: "123"},
+			},
+		},
+		{
+			input: xerrors.Errorf(
+				"something went wrong: %w",
+				errors.ErrSyntax("some error", &token.Token{Value: "123"})),
+			expected: &SyntaxError{
+				Msg:   "some error",
+				Token: &token.Token{Value: "123"},
+			},
+		},
+	}
+	for _, test := range tests {
+		syntaxErr := AsSyntaxError(test.input)
+		if test.expected == nil {
+			if syntaxErr != nil {
+				t.Fatalf("wanted nil, but go %v", syntaxErr)
+			}
+			return
+		}
+		if *test.expected.Token != *syntaxErr.Token && test.expected.Msg != syntaxErr.Msg {
+			t.Fatalf("unexpected output.\nexpect:\n[%v]\nactual:\n[%v]", test.expected, syntaxErr)
+		}
+	}
+}

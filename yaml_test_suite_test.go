0e95011a5dc7a437c31706dcf7840e0faa225ff0
Masaaki Goshima <goccy54@gmail.com>
2024-12-09 22:14:28 +0900

    Fix parsing of multiline mapping key (#579)
    * fix multiline mapping key
    
    * remove passed test cases
    
    * add boundary checking

--- yaml_test_suite_test.go
+++ yaml_test_suite_test.go
@@ -37,21 +37,17 @@ var failureTestNames = []string{
 	"invalid-comment-after-comma",
 	"invalid-comment-after-end-of-flow-sequence",
 	"invalid-comma-in-tag",
-	"invalid-tag",                                // pass yamlv3.
-	"legal-tab-after-indentation",                // pass yamlv3.
-	"literal-modifers/00",                        // pass yamlv3.
-	"literal-modifers/01",                        // pass yamlv3.
-	"literal-modifers/02",                        // pass yamlv3.
-	"literal-modifers/03",                        // pass yamlv3.
-	"literal-scalars",                            // pass yamlv3.
-	"mapping-key-and-flow-sequence-item-anchors", // no json.
-	"multiline-double-quoted-implicit-keys",      // pass yamlv3.
-	"multiline-plain-flow-mapping-key",
+	"invalid-tag",                                    // pass yamlv3.
+	"legal-tab-after-indentation",                    // pass yamlv3.
+	"literal-modifers/00",                            // pass yamlv3.
+	"literal-modifers/01",                            // pass yamlv3.
+	"literal-modifers/02",                            // pass yamlv3.
+	"literal-modifers/03",                            // pass yamlv3.
+	"literal-scalars",                                // pass yamlv3.
+	"mapping-key-and-flow-sequence-item-anchors",     // no json.
 	"multiline-plain-value-with-tabs-on-empty-lines", // pass yamlv3.
 	"multiline-scalar-at-top-level",                  // pass yamlv3.
 	"multiline-scalar-at-top-level-1-3",              // pass yamlv3.
-	"multiline-single-quoted-implicit-keys",          // pass yamlv3.
-	"multiline-unidented-double-quoted-block-key",    // pass yamlv3.
 	"nested-implicit-complex-keys",                   // no json.
 	"node-anchor-not-indented",                       // pass yamlv3.
 	"plain-dashes-in-flow-sequence",
